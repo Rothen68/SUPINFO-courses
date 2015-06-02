bcd4ebe63c02d3fab649b422d48213082f51fbea
Robin <robin.choffardet@ikarnet.com>
2015-06-02 17:12:05 +0200

    first lesson bis

---
+++ 2WEB/TP#3-4-5 Al-Bhed translator/index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<title>Al-Bhed Translator</title>
+</head>
+<body>
+	<h1>Al-Bhed Translator</h1>
+	<form action="./translate.php" method="post">
+		<label for="string">Word to translate:</label>
+		<input type="text" id="string" name="string" value="" />
+		<input type="submit" value="submit" />
+	</form>
+</body>
+</html>
--- 2WEB/TP#3-4-5 Al-Bhed translator/translate.php
+++ 2WEB/TP#3-4-5 Al-Bhed translator/translate.php
@@ -1,16 +1,22 @@
 <?php
+
+include('index.php');
+
 function translate($str)
 {
-	$chars = str_split('YPLTAVKREZGMSHUBXNCDIJFQOW');
 	$str = str_split(strtoupper($str));
+	$chars = str_split('YPLTAVKREZGMSHUBXNCDIJFQOW');
 
 	$buffer = '';
-	foreach ($str as $c) 
+	if(count($str) == 0)
 	{
-		$buffer .= $chars[ord($c)-65];
+		foreach ($str as $c) 
+		{
+			$buffer .= $chars[ord($c) - 65];
+		}
 	}
 
 	return $buffer;
 }
 
-echo translate('apricot');
+echo translate(empty($_POST['content']) ? '' : $_POST['content']);
